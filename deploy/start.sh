1b28ee56dedbe07294b8e0707d6ee7e8e16f11f8
DamianM <damianswitch@hotmail.com>
2026-04-10 16:48:05 -0400

    ajustes persistencia de datos

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 chmod -R 777 /var/www/public/img
 chmod -R 777 /var/www/public/files
+chmod -R 777 /var/www/writable
 
 chown -R www-data:www-data /var/www/public/img
 chown -R www-data:www-data /var/www/public/files
-
+chown -R www-data:www-data /var/www/writable
 php-fpm -D
 nginx -g "daemon off;"
